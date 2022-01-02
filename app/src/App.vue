6df8097f594939aec40bc6cac9fed46f497221f0
Michael Cole <mcole042891.prof.dev@gmail.com>
2022-01-02 13:37:33 -0600

    Moved navbar to separate component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,59 +1,17 @@
 <template>
 
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
+  <Navbar />
 
-        <!-- Home Link -->
-        <router-link class="navbar-brand" to="/">Nuka Cole</router-link>
-        <button
-          class="navbar-toggler" type="button" data-bs-toggle="collapse"
-          v-bind:class="{active: isPage('Home')}"
-          data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- Other Links -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <!-- Login Link -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link" v-bind:class="{active: isPage('Login')}"
-                to="/login">Login</router-link>
-            </li>
-            <!-- Minibank Link -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link" v-bind:class="{active: isPage('Minibank')}"
-                to="/minibank">Mini Bank</router-link>
-            </li>
-            <!-- Task Manager Link -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link" v-bind:class="{active: isPage('Taskmanager')}"
-                to="/taskmanager">Task Manager</router-link>
-            </li>
-            <!-- About Link -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link" v-bind:class="{active: isPage('About')}"
-                to="/about">About</router-link>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-    </nav>
-  </div>
-
-  <router-view/>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+
 export default {
   name: 'App',
+  components: {
+    Navbar,
+  },
   methods: {
     isPage(pagename) {
       // Returns true if pagename matches the routename
---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,83 @@
+<template>
+
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+
+        <!-- Home Link -->
+        <router-link class="navbar-brand" to="/">Nuka Cole</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          v-bind:class="{ active: isPage('Home') }"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Other Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!-- Login Link -->
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                v-bind:class="{ active: isPage('Login') }"
+                to="/login"
+                >Login</router-link
+              >
+            </li>
+            <!-- Minibank Link -->
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                v-bind:class="{ active: isPage('Minibank') }"
+                to="/minibank"
+                >Mini Bank</router-link
+              >
+            </li>
+            <!-- Task Manager Link -->
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                v-bind:class="{ active: isPage('Taskmanager') }"
+                to="/taskmanager"
+                >Task Manager</router-link
+              >
+            </li>
+            <!-- About Link -->
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                v-bind:class="{ active: isPage('About') }"
+                to="/about"
+                >About</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    isPage(pagename) {
+      // Returns true if pagename matches the routename
+      return pagename === this.$route.name;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
